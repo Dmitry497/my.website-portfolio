371ed376dacbc6702dbcc94dbc664ff1e31b223e
Dmitriy <dmitry.githubakkount@gmail.com>
2026-03-31 23:48:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ class Starfield {
         this.animate();
         window.addEventListener('resize', () => this.resize());
     }
-    
+
     init() {
         this.resize();
         this.createStars();
